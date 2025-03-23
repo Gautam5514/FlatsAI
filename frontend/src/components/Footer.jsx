352a91870443d163e58309f1631bea3d75106337
Gautam pandit <gautampandit797@gmail.com>
2025-03-23 21:29:42 +0530

    Added Some message

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                     {/* Company Info */}
                     <div className="mb-8 md:mb-0">
                         <h3 className="text-xl text-black font-bold mb-4">Real Estate Co.</h3>
-                        <p className="text-red-700">
+                        <p className="text-gray-700">
                             Your trusted partner in finding the perfect home. We specialize in residential, commercial, and rental properties.
                         </p>
                     </div>
@@ -16,7 +16,7 @@ const Footer = () => {
                     {/* Quick Links */}
                     <div className="mb-8 md:mb-0">
                         <h4 className="text-lg text-black font-semibold mb-4">Quick Links</h4>
-                        <ul className="text-red-700">
+                        <ul className="text-gray-800">
                             <li className="mb-2"><a href="/about" className="hover:text-white">About Us</a></li>
                             <li className="mb-2"><a href="/listings" className="hover:text-white">Listings</a></li>
                             <li className="mb-2"><a href="/services" className="hover:text-white">Services</a></li>
@@ -27,7 +27,7 @@ const Footer = () => {
                     {/* Contact Info */}
                     <div className="mb-8 md:mb-0">
                         <h4 className="text-lg text-black font-semibold mb-4">Contact Info</h4>
-                        <ul className="text-red-700">
+                        <ul className="text-gray-700">
                             <li className="mb-2"> Sector 2, Greater Noida West, Uttar Pradesh, India</li>
                             <li className="mb-2">Phone: +91 9031036321</li>
                             <li className="mb-2">Email: info@realestateco.com</li>
@@ -38,16 +38,16 @@ const Footer = () => {
                     <div>
                         <h4 className="text-lg text-black font-semibold mb-4">Follow Us</h4>
                         <div className="flex space-x-4">
-                            <a href="https://facebook.com" className="text-blue-400 hover:text-white">
+                            <a href="https://www.facebook.com/profile.php?id=61570928146428" className="text-blue-400 hover:text-white">
                                 <i className="fab fa-facebook-f"></i>
                             </a>
-                            <a href="https://twitter.com" className="text-blue-500 hover:text-white">
+                            <a href="https://x.com/Flats_AI?t=B_zQfDgejsW6EZdjWNHcDg&s=08" className="text-blue-500 hover:text-white">
                                 <i className="fab fa-twitter"></i>
                             </a>
-                            <a href="https://instagram.com" className="text-red-400 hover:text-white">
+                            <a href="https://www.instagram.com/flats_ai?igsh=Y2FjaWF4OWZxYWxs" className="text-red-400 hover:text-white">
                                 <i className="fab fa-instagram"></i>
                             </a>
-                            <a href="https://linkedin.com" className="text-blue-700 hover:text-white">
+                            <a href="https://www.linkedin.com/in/connect-suraj/" className="text-blue-700 hover:text-white">
                                 <i className="fab fa-linkedin-in"></i>
                             </a>
                         </div>
