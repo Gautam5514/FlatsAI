c340cc234b3b30de0137e71cdec456ad47aacd93
Gautam pandit <gautampandit797@gmail.com>
2025-03-29 22:41:16 +0530

    added some img

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,7 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
+app.listen()
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
- 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
@@ -31,7 +30,7 @@ import Login from "./pages/Login";
 import ProtectedRoute from "./pages/ProtectedRouts";
 
 function App() {
- 
+
 
   return (
     <Router>
